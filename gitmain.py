130352c58095ac33674191d4b2c42965af9cde59
PringoleonLeRetromancien <120843444+PringoleonLeRetromancien@users.noreply.github.com>
2023-02-26 10:38:29 +0100

    Màj de main.py

--- gitmain.py
+++ gitmain.py
@@ -1,20 +1,17 @@
 import praw 
 import urllib.request
-import os 
 
-#reddit = praw.Reddit(client_id='voire README', 
-	#client_secret='voir README', 
-	#user_agent='voir README')
+reddit = praw.Reddit(client_id='voire README', 
+	client_secret='voir README', 
+	user_agent='voir README')
 
-#subreddit = reddit.subreddit('') #ajoute le nom des subreddits sans le r/ et avec des plus pour en mettre plusieurs dans la chaine (string) // add the names of the subreddits without the r/ and with + to add multiple subreddit (one string)
-path=os.path.abspath(".")
-os.makedirs(path)
+subreddit = reddit.subreddit('voir README') #ajoute le nom des subreddits sans le r/ et avec des plus pour en mettre plusieurs dans la chaine (string) // add the names of the subreddits without the r/ and with + to add multiple subreddit (one string)
 
-with open("\\images\\url.txt", "a") as file:
-    print("hello world")
-    #for post in subreddit.top(limit=5):
-        #if not post.is_self and post.url.endswith(('.jpg', '.jpeg', '.png')):
-            #url = post.url
-            #file.write(url + '\n')
-            #file_name = url.split("/")[-1]
-            #urllib.request.urlretrieve(url, "\\images\\" + file_name)
+
+with open("voir README", "a") as file:
+    for post in subreddit.top(limit=5):
+        if not post.is_self and post.url.endswith(('.jpg', '.jpeg', '.png')):
+            url = post.url
+            file.write(url + '\n')
+            file_name = url.split("/")[-1]
+            urllib.request.urlretrieve(url, "voir README" + file_name) #la fonction qui télécharge/ the downloading function
